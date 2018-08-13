abfbab6cac31c974bc8246481e0c2f310b301417
YuKongEr <541130126@qq.com>
2018-08-13 16:38:38 +0800

    SpringBoot之使用thymeleaf作为视图模板

---
+++ chapter2-1/src/main/java/com/yukong/chapter21/IndexController.java
@@ -0,0 +1,23 @@
+package com.yukong.chapter21;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Auther: yukong
+ * @Date: 2018/8/13 13:58
+ * @Description: SpringBoot集成jsp
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "world", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
---
+++ chapter2-1/src/test/java/com/yukong/chapter21/Chapter21ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yukong.chapter21;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter21ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
